fa5b3cb13867023d17c7be112b2bad7e339b0e86
echizen92 <domo.arigato@live.com>
2019-05-21 14:51:36 +0800

    Add files via upload

---
+++ GetUserLocation/SearchResultsController.swift
@@ -0,0 +1,94 @@
+import UIKit
+import GoogleMaps
+import GooglePlaces
+import Alamofire
+import SwiftyJSON
+
+
+
+
+protocol LocateOnTheMap{
+    func locateWithLongitude(_ lon:Double, andLatitude lat:Double, andTitle title: String)
+}
+
+class SearchResultsController: UITableViewController{
+    let googleAPIKey = "AIzaSyCihv-fGphTYd6g9ojb3IYYKooF0q1Y-WQ"
+    var searchResults: [String]!
+    var delegate: LocateOnTheMap!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.searchResults = Array()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.searchResults.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = self.searchResults[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath)
+    {
+        self.dismiss(animated: true, completion: nil)
+        let urlpath = "https://maps.googleapis.com/maps/api/geocode/json?address=\(self.searchResults[indexPath.row])&sensor=false".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let url = URL(string: urlpath!)
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) -> Void in
+            do {
+                if data != nil{
+                    let dic = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! NSDictionary
+                    let status = dic.value(forKey: "status") as! String
+                   // print("Error")
+                    print("\(status)")
+                   if status == "OK"
+                    {
+                        
+                        let lat =   (((((dic.value(forKey: "results") as! NSArray).object(at: 0) as! NSDictionary).value(forKey: "geometry") as! NSDictionary).value(forKey: "location") as! NSDictionary).value(forKey: "lat")) as? Double
+                        let lon =   (((((dic.value(forKey: "results") as! NSArray).object(at: 0) as! NSDictionary).value(forKey: "geometry") as! NSDictionary).value(forKey: "location") as! NSDictionary).value(forKey: "lng")) as? Double
+                        if let latitude = lat
+                        {
+                            if let longitude = lon
+                            {
+                                self.delegate.locateWithLongitude(longitude, andLatitude: latitude, andTitle: self.searchResults[indexPath.row])
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                print("Error")
+            }
+        }
+        task.resume()
+    }
+    
+
+    
+    
+    func reloadDataWithArray(_ array:[String]){
+        self.searchResults = array
+        self.tableView.reloadData()
+}
+}
--- GetUserLocation/ViewController.swift
+++ GetUserLocation/ViewController.swift
@@ -5,14 +5,49 @@
 //  Created by Faiz Rashid on 11/5/19.
 //  Copyright © 2019 Faiz Rashid. All rights reserved.
 //
-
 import UIKit
+import GooglePlaces
 import GoogleMaps
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate,LocateOnTheMap, GMSAutocompleteFetcherDelegate {
+    
+    func locateWithLongitude(_ lon: Double, andLatitude lat: Double, andTitle title: String) {
+        
+    }
+    
+    
+    public func didFailAutocompleteWithError(_ error: Error) {
+        //        resultText?.text = error.localizedDescription
+    }
+    
+    /**
+     * Called when autocomplete predictions are available.
+     * @param predictions an array of GMSAutocompletePrediction objects.
+     */
+    public func didAutocomplete(with predictions: [GMSAutocompletePrediction]) {
+        //self.resultsArray.count + 1
+        
+        for prediction in predictions {
+            
+            if let prediction = prediction as GMSAutocompletePrediction?{
+                self.resultsArray.append(prediction.attributedFullText.string)
+            }
+        }
+        self.searchResultController.reloadDataWithArray(self.resultsArray)
+//        self.searchResultsTable.reloadDataWithArray(self.resultsArray)
+        print(resultsArray)
+    }
 
-    @IBOutlet weak var mapView: GMSMapView!
+    
+    @IBOutlet weak var mapView: UIView!
+    
+    var googleMapsView :GMSMapView!
+    var searchResultController: SearchResultsController!
+    var resultsArray = [String]()
+    var gmsFetcher: GMSAutocompleteFetcher!
+  
+    
     private let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -20,17 +55,84 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
-      
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        self.googleMapsView = GMSMapView(frame: self.mapView.frame)
+        self.view.addSubview(self.googleMapsView)
+        
+        searchResultController = SearchResultsController()
+        searchResultController.delegate = self
+        gmsFetcher = GMSAutocompleteFetcher()
+        gmsFetcher.delegate = self
+    }
+    
+    //action for search location by address
+    
+    @IBAction func searchWithAddress(_ sender: Any) {
+      let searchController = UISearchController(searchResultsController: searchResultController)
+    
+        searchController.searchBar.delegate = self
+        
+        self.present(searchController, animated: true, completion: nil)
+    }
+    
+    func locateWithLong(lon: String, andLatitude lat: String, andAddress address: String) {
+        DispatchQueue.main.async {
+            
+            let latDouble = Double(lat)
+            let lonDouble = Double(lon)
+            self.googleMapsView.clear()
+            let position = CLLocationCoordinate2D(latitude: latDouble ?? 20.0, longitude: lonDouble ?? 10.0)
+            let marker = GMSMarker(position: position)
+            let camera = GMSCameraPosition.camera(withLatitude: latDouble ?? 20.0, longitude: lonDouble ?? 10.0, zoom: 15)
+            self.googleMapsView.camera = camera
+            marker.title = "Address: \(String(describing: self.title)))"
+            //marker.title = "Address : \(String(describing: self.title))"
+            marker.map = self.googleMapsView
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        //        let placeClient = GMSPlacesClient()
+        //
+        //
+        //        placeClient.autocompleteQuery(searchText, bounds: nil, filter: nil)  {(results, error: Error?) -> Void in
+        //           // NSError myerr = Error;
+        //            print("Error @%",Error.self)
+        //
+        //            self.resultsArray.removeAll()
+        //            if results == nil {
+        //                return
+        //            }
+        //
+        //            for result in results! {
+        //                if let result = result as? GMSAutocompletePrediction {
+        //                    self.resultsArray.append(result.attributedFullText.string)
+        //                }
+        //            }
+        //
+        //            self.searchResultController.reloadDataWithArray(self.resultsArray)
+        //
+        //        }
+        
+        
+        self.resultsArray.removeAll()
+        gmsFetcher?.sourceTextHasChanged(searchText)
         
         
     }
     
-   // func TargetCoordinateGeofence(){
-        
-   // }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
 
 // MARK: - CLLocationManagerDelegate
@@ -46,8 +148,8 @@ extension ViewController: CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         
         //5
-        mapView.isMyLocationEnabled = true
-        mapView.settings.myLocationButton = true
+        googleMapsView.isMyLocationEnabled = true
+        googleMapsView.settings.myLocationButton = true
     }
     
     // 6
@@ -57,10 +159,9 @@ extension ViewController: CLLocationManagerDelegate {
         }
         
         // 7
-        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+        googleMapsView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
         
         // 8
         locationManager.distanceFilter = 5
-    }
 }
-
+}
